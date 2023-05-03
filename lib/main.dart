b577000911e6580ade445b820a26ed972e70a208
AON2 <support@aon2.co.uk>
2023-05-03 18:18:44 +0100

    fix errno = 22. need to open port before reading.

--- lib/main.dart
+++ lib/main.dart
@@ -50,21 +50,21 @@ class _ExampleAppState extends State<ExampleApp> {
             children: [
               for (final address in availablePorts)
                 Builder(builder: (context) {
-                  final port = SerialPort(address);
+                  // final port = SerialPort(address);
                   return ExpansionTile(
                     title: Text(address),
-                    children: [
-                      CardListTile('Description', port.description),
-                      CardListTile('Transport', port.transport.toTransport()),
-                      CardListTile('USB Bus', port.busNumber?.toPadded()),
-                      CardListTile('USB Device', port.deviceNumber?.toPadded()),
-                      CardListTile('Vendor ID', port.vendorId?.toHex()),
-                      CardListTile('Product ID', port.productId?.toHex()),
-                      CardListTile('Manufacturer', port.manufacturer),
-                      CardListTile('Product Name', port.productName),
-                      CardListTile('Serial Number', port.serialNumber),
-                      CardListTile('MAC Address', port.macAddress),
-                    ],
+                    // children: [
+                    //   CardListTile('Description', port.description),
+                    //   CardListTile('Transport', port.transport.toTransport()),
+                    //   CardListTile('USB Bus', port.busNumber?.toPadded()),
+                    //   CardListTile('USB Device', port.deviceNumber?.toPadded()),
+                    //   CardListTile('Vendor ID', port.vendorId?.toHex()),
+                    //   CardListTile('Product ID', port.productId?.toHex()),
+                    //   CardListTile('Manufacturer', port.manufacturer),
+                    //   CardListTile('Product Name', port.productName),
+                    //   CardListTile('Serial Number', port.serialNumber),
+                    //   CardListTile('MAC Address', port.macAddress),
+                    // ],
                   );
                 }),
             ],
