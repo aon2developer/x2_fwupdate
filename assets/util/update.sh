bf7e014914c2c372899fb1e80629bcf9aef15f9c
aon2developer <developer@aon2.co.uk>
2023-07-15 12:48:26 +0100

    Returned to listed devices display

--- assets/util/update.sh
+++ assets/util/update.sh
@@ -3,29 +3,34 @@
 # Ensure that the scipt executes in current directory
 cd "$(dirname "$0")"
 
-# TODO: implement infinite loop/scan (or loop/scan for limited time before needing to "refresh") until found X2
-# Find X2 dev path
-for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-    devpath=$(
-        syspath="${sysdevpath%/dev}"
-        devname="$(udevadm info -q name -p $syspath)"
-        # Stop if device is not an acm device (+filtering)
-        [[ "$devname" != *"ttyACM"* ]] && exit
-        eval "$(udevadm info -q property --export -p $syspath)"
-        # Return if X2 match found
-        [[ "$ID_VENDOR_ID" == "16d0" && "$ID_MODEL_ID" == "0cc4" ]] && devpath="/dev/$devname"
-        echo $devpath
-    )
-    echo $devpath
-    [[ "$devpath" != "" ]] && break
-done
+# # TODO: implement infinite loop/scan (or loop/scan for limited time before needing to "refresh") until found X2
+# # Find X2 dev path
+# for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+#     devpath=$(
+#         syspath="${sysdevpath%/dev}"
+#         devname="$(udevadm info -q name -p $syspath)"
+#         # Stop if device is not an acm device (+filtering)
+#         [[ "$devname" != *"ttyACM"* ]] && exit
+#         eval "$(udevadm info -q property --export -p $syspath)"
+#         # Return if X2 match found
+#         [[ "$ID_VENDOR_ID" == "16d0" && "$ID_MODEL_ID" == "0cc4" ]] && devpath="/dev/$devname"
+#         echo $devpath
+#     )
+#     echo $devpath
+#     [[ "$devpath" != "" ]] && break
+# done
 
-[[ "$devpath" == "" ]] && echo "X2 not found, make sure that it's plugged in!" & exit
-echo $devpath
+# [[ "$devpath" == "" ]] && echo "X2 not found, make sure that it's plugged in!" & exit
+# echo $devpath
 
-# Put X2 into update mode 
-stty -F "$devpath" 1200
+# Put X2 into bootloader mode 
+echo "Going into bootloader mode..."
+stty -F "/dev/ttyACM0" 1200 # TODO: message the user to unplug any device that my come up as ttyACM0
 sleep 5
+echo "Successfully activated bootloader mode!"
 
 # Execute update utility
+echo "Beginning update..."
 ./dfu-util-linux -d 0x16D0:0x0CC4,0x0483:0xdf11 -a 0 -s 0x08000000:leave -D ../firmware/X2-1.3.6.dfu
+
+echo "Done!"
